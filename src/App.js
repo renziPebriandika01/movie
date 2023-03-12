17c141db5ef85b2acbd61514660dd8c832d0a838
renziPebriandika01 <renzspectorlimit@gmail.com>
2023-03-12 22:34:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { getMovieList,movieSearch }from './api';
+import { useEffect, useState } from 'react';
+
+const App =()=> {
+  const [popularMovies,setPopularMovies]= useState ([])
+
+  useEffect(()=>{
+    getMovieList().then((result)=>{
+      setPopularMovies(result)
+    })
+  },[])
+
+
+  const search = ({q})=>{
+    console.log({q})
+  }
+
+const PopularMovieList= ()=>{
+  return popularMovies.map((movie,i)=>{
+    return (
+   
+      <div className="Movie-wrapper" key= {i}> 
+          <div className="Movie-tittle">{movie.title}</div>
+          <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}/>
+          <div className="Movie-date">{movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+      </div>
+
+    
+  
+    )
+  })
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Movie-Search</h1>
+      <input placeholder='cari film...' className="Movie-search" 
+      onChange={({target})=>search(target.value)}/>
+   <div className="Movie-container">
+    <PopularMovieList/>
+    </div>
       </header>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const apiKey  =process.env.REACT_APP_APIKEY
+const baseUrl =process.env.REACT_APP_BASEURL
+
+// export const getMovieList = async () => {
+//     const movie = await axios.get (`https://api.themoviedb.org/3/movie/popular?api_key=cd55c8384de115a4db89ba967dae48ae`)
+//     console.log({getMovieList:movie})
+// }
+export const getMovieList = async () => {
+    const movie = await axios.get (`${baseUrl}/movie/popular?api_key=${apiKey}`)
+return movie.data.results
+
+}
+
+
+export const movieSearch = async (q) => {
+    const search = await axios.get(q)
+    return
+}
